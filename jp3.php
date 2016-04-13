eb576528d1ee47974e4406e2bc07b292f3b9dd7f
egoing <egoing@gmail.com>
2016-04-13 10:46:13 +0900

    숫자

---
+++ jp3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>JavaScript와 PHP</h1>
+    <h2>숫자</h2>
+    <script>
+      document.write(1+1);
+    </script>
+    <h2>PHP</h2>
+    <?php
+      print(1+1);
+    ?>
+  </body>
+</html>
