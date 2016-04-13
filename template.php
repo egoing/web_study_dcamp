7feaac6a4adda38b1add7d06f622183d79aaf23a
egoing <egoing@gmail.com>
2016-04-13 10:45:53 +0900

    예제를 위한 템플릿 소스코드

---
+++ template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>JavaScript와 PHP</h1>
+    <h2>JavaScript</h2>
+    <script>
+
+    </script>
+    <h2>PHP</h2>
+    <?php
+      
+    ?>
+  </body>
+</html>
