a8587cebc83d1771fb270830e41e1c0fee6e6159
egoing <egoing@gmail.com>
2016-04-13 10:46:41 +0900

    변수

---
+++ jp5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>변수(Variable)</h1>
+    <h2>JavaScript</h2>
+    <script>
+      var a = 2;
+      var b = 1;
+      document.write(a+b);
+    </script>
+    <h2>PHP</h2>
+    <?php
+      $a = 2;
+      $b = 1;
+      print($a+$b);
+    ?>
+  </body>
+</html>
