96ad0173efc1840a9cb9cd34001892f23df0afea
egoing <egoing@gmail.com>
2016-04-13 10:45:37 +0900

    자바스크립트와 php

---
+++ jp1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>JavaScript와 PHP</h1>
+    <h2>JavaScript</h2>
+    <script>
+      document.write(1+1);
+    </script>
+    <h2>PHP</h2>
+    <?php
+      print(1+1);
+    ?>
+  </body>
+</html>
