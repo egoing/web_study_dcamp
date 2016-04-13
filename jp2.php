9505ccf22d16a868e3aa8217d8ff9386a440e7c4
egoing <egoing@gmail.com>
2016-04-13 10:46:06 +0900

    문자열

---
+++ jp2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>문자열(String)</h1>
+    <h2>JavaScript</h2>
+    <script>
+      document.write('Hello world');
+    </script>
+    <h2>PHP</h2>
+    <?php
+      print('Hello world');
+    ?>
+  </body>
+</html>
