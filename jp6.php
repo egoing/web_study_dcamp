9f86e06a8079bbef4b428b337651e7ce87315995
egoing <egoing@gmail.com>
2016-04-14 18:59:38 +0900

    boolean

---
+++ jp6.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>JavaScript와 PHP</h1>
+    <h2>JavaScript</h2>
+    <script>
+      document.write(1 < 2);
+    </script>
+    <h2>PHP</h2>
+    <?php
+      var_dump(1 < 1);
+    ?>
+  </body>
+</html>
