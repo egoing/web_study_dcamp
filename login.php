af0e407286b07a6321751bb81345bb6be3a1590d
egoing <egoing@gmail.com>
2016-04-14 19:40:42 +0900

    조건문의 활용 : 로그인 에플리케이션

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script>
+      var 입력한비밀번호 = prompt('비밀번호는요?');
+      var 사용자의비밀번호 = 123;
+      if(입력한비밀번호 === 사용자의비밀번호) {
+        alert('주인님 어서오세요');
+      }
+
+
+
+      //var password = prompt('비밀번호는요?');
+      //alert('비밀번호는 '+password+'입니다.');
+
+
+    </script>
+  </body>
+</html>
