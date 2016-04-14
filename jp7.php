4d68e5b93558b21d4b7a5335316b7ec3c962f264
egoing <egoing@gmail.com>
2016-04-14 19:40:28 +0900

    조건문의형식

---
+++ jp7.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>조건문</h1>
+    <h2>JavaScript</h2>
+    <script>
+      var result = true;
+      if(result){
+        document.write('Hi, if');
+      }
+    </script>
+    <h2>PHP</h2>
+    <?php
+      if(false){
+        print('Hi, if');
+      }
+    ?>
+  </body>
+</html>
