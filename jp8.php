5a1878b43187fa8fd6c22f3fa12247ea72fa1ab8
egoing <egoing@gmail.com>
2016-04-14 20:24:43 +0900

    반복문

---
+++ jp8.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>반복문</h1>
+    <h2>JavaScript</h2>
+    <script>
+      var i = 0;
+      while(i < 5){
+        document.write('<li>Hello world</li>');
+        i = i + 1;
+      }
+    </script>
+    <h2>PHP</h2>
+    <?php
+      $i = 0;
+      while($i < 5){
+        print('<li>Hello world</li>');
+        $i = $i + 1;
+      }
+
+    ?>
+
+
+
+
+
+
+
+  </body>
+</html>
--- login.php
+++ login.php
@@ -8,16 +8,11 @@
     <script>
       var 입력한비밀번호 = prompt('비밀번호는요?');
       var 사용자의비밀번호 = 123;
-      if(입력한비밀번호 === 사용자의비밀번호) {
+      if(입력한비밀번호 == 사용자의비밀번호) {
         alert('주인님 어서오세요');
+      } else {
+        alert('뉘슈?');
       }
-
-
-
-      //var password = prompt('비밀번호는요?');
-      //alert('비밀번호는 '+password+'입니다.');
-
-
     </script>
   </body>
 </html>
