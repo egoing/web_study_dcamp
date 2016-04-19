bfb307355a04a57eb9c556450f5c6e0d3eddd97c
egoing <egoing@gmail.com>
2016-04-19 20:18:07 +0900

    배열을 이용한 php 예제

--- index.php
+++ index.php
@@ -1,30 +1,29 @@
-<!DOCTYPE html>
+<?php
+$id = $_GET['id'];
+$article1 = array('title'=>'변수와 상수', 'description'=>'변수는...');
+$article2 = array('title'=>'조건문', 'description'=>'조건문은...');
+$article3 = array('title'=>'반복문', 'description'=>'반복문은...');
+$article4 = array('title'=>'함수', 'description'=>'함수는...');
+$list = array($article1, $article2, $article3, $article4);
+?>
+
 <html>
 <head>
-  <title>변수와 상수</title>
   <meta charset="utf-8">
-  <meta name="author" contents="egoing">
-  <link rel="stylesheet" href="style.css">
+  <title><?php print($list[$id]['title']); ?></title>
 </head>
 <body>
-  <h1><a href="index.html">JavaScript</a></h1>
-  <ul>
-    <li><a href="index.php?id=1">변수와 상수</a></li>
-    <li><a href="index.php?id=2">조건문</a></li>
-    <li><a href="index.php?id=3">반복문</a></li>
-  </ul>
-  <article>
-    <?php
-    if($_GET['id'] == 1) {
-      print('<h2>변수와 상수</h2>변수와 상수는..');
-    }
-    if($_GET['id'] == 2) {
-      print('<h2>조건문</h2>조건문..');
-    }
-    if($_GET['id'] == 3) {
-      print('<h2>반복문</h2>반복문은..');
-    }
-    ?>
-  </article>
+<?php
+print('<ol>');
+$i = 0;
+while($i < count($list)){
+  print('<li><a href="index.php?id='.$i.'">'.$list[$i]['title'].'</a></li>');
+  $i = $i + 1;
+}
+print('</ol>');
+
+print('<h3>'.$list[$id]['title'].'</h3>');
+print($list[$id]['description']);
+?>
 </body>
 </html>
