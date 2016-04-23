f424f0b5ee5316055961b7f17df6fd9e17836d0d
egoing <egoing@gmail.com>
2016-04-23 16:06:10 +0900

    mysql - php 연동예제

--- index.php
+++ index.php
@@ -1,50 +1,36 @@
-<?php
-$article1 = array('title'=>'변수와 상수', 'description'=>'변수는...');
-$article2 = array('title'=>'조건문', 'description'=>'조건문은...');
-$article3 = array('title'=>'반복문', 'description'=>'반복문은...');
-$article4 = array('title'=>'함수', 'description'=>'함수는...');
-$list = array($article1, $article2, $article3, $article4);
-if(empty($_GET['id'])){
-  $title = "Welcome";
-  $description = "Hello";
-} else {
-  $title = $list[$_GET['id']]['title'];
-  $description = $list[$_GET['id']]['description'];
-}
-?>
-
+<!DOCTYPE>
 <html>
 <head>
-  <meta charset="utf-8">
-  <title><?php print($title); ?></title>
-  <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
-<body class="white" id="target">
+<body>
   <h1><a href="index.php">JavaScript</a></h1>
 <?php
+$link = mysqli_connect('localhost', 'root', '111111', 'opentutorials');
+$result = mysqli_query($link, "SELECT * FROM topic");
+
+print('<nav>');
 print('<ul>');
-$i = 0;
-while($i < count($list)){
-  print('<li><a href="index.php?id='.$i.'">'.$list[$i]['title'].'</a></li>');
-  $i = $i + 1;
+while($row = mysqli_fetch_assoc($result)){
+  print('<li><a href="index.php?id='.$row['id'].'">'.$row['title'].'</a></li>');
 }
 print('</ul>');
+print('</nav>');
+
+if(isset($_GET['id'])) {
+  $id = $_GET['id'];
+  $result = mysqli_query($link, "SELECT * FROM topic WHERE id=".$id);
+  $article = mysqli_fetch_assoc($result);
+  $title = $article['title'];
+  $description = $article['description'];
+} else {
+  $title = 'Welcome';
+  $description = 'JavaScript is good.';
+}
+print('<article>');
+print('<h2>'.$title.'</h2>');
+print($description);
+print('</article>');
 ?>
-<article>
-  <input type="button" value="white" onclick="document.getElementById('target').className='white'">
-  <input type="button" value="black" onclick="document.getElementById('target').className='black'">
-
-<h3><?php print($title); ?></h3>
-<?php print($description); ?>
-</article></body>
-
-
-
-
-
-
-
-
-
-
+</body>
 </html>
